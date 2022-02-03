06d01a9ca75d2d007e2f0799ac58a7326c905bc5
Peter Darinzo <birel44@yahoo.com>
2022-02-03 09:07:54 -0800

    new repo for backend

---
+++ db.js
@@ -0,0 +1,48 @@
+"use strict";
+/** Database setup for jobly. */
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+
+let db;
+
+/** Springboard Method */
+if (process.env.NODE_ENV === "production") {
+  db = new Client({
+    connectionString: getDatabaseUri(),
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  db = new Client({
+    connectionString: getDatabaseUri()
+  });
+}
+/** END Springboard Method */
+
+
+/** My Method */
+// if (process.env.NODE_ENV === "production") {
+//   db = new Client({
+//     host: "localhost",
+//     user: "",
+//     password: "",
+//     database: getDatabaseUri(),
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   });
+// } else {
+//   db = new Client({
+//     host: "localhost",
+//     user: "",
+//     password: "",
+//     database: getDatabaseUri()
+//   })
+// }
+/** END My Method */
+
+
+db.connect();
+
+module.exports = db;
